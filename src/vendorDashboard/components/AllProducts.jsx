c77ef423eb6ef46507555b3d805bba5a50d9d2e2
AkashReddy1010 <reddyakash731@gmail.com>
2025-04-08 09:05:59 +0530

    react_vendor_dashboard

---
+++ src/vendorDashboard/components/AllProducts.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import { API_URL } from '../helpers/ApiPath';
+
+const AllProducts = () => {
+    const [products,setProducts] = useState([]);
+
+    const productsHandler = async() => {
+        const firmId = localStorage.getItem('firmId');
+        try {
+            const response = await fetch(`${API_URL}/product/${firmId}/products`);
+            const newProductsData = await response.json();
+            setProducts(newProductsData.products);
+            console.log(newProductsData);
+            
+        } catch (error) {
+            console.error("Failed to fetch products", error);
+            alert("Failed to fetch products");
+        }
+    }
+
+    useEffect(()=>{
+        productsHandler();
+    },[])
+
+    const deleteProductById = async(productId)=>{
+        console.log(productId)
+        try {
+            const response = await fetch(`${API_URL}/product/${productId}`,{
+                method: "DELETE",
+                headers: {
+                //   'Authorization': `Bearer ${token}`, // if needed
+                  'Content-Type': 'application/json'
+                }
+            })
+            const data = await response.json()
+            console.log(data)
+            if(response.ok){
+                setProducts(products.filter(product=>product._id === productId));
+                confirm("Are you sure, you want to delete?")
+                alert("Product deleted successfully")
+            }
+        } catch (error) {
+            console.error("Failed to delete product");
+            alert("Failed to delete product");
+        }
+    }
+
+  return (
+    <div className='AllProduct'>
+      {!products ?(
+        <p>No Products added</p>
+      ) : (
+        <table className="product-table">
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((item)=>{
+                    return (
+                        <>
+                            <tr key={item._id}>
+                                <td>{item.productName}</td>
+                                <td>{item.price}</td>
+                                <td>
+                                    {item.image && (
+                                        <img src={`${API_URL}/uploads/${item.image}`} alt={item.productName}
+                                        style={{width:"50px",height:"50px"}}
+                                        />
+                                    )}
+                                </td>
+                                <td>
+                                    <button onClick={()=>deleteProductById(item._id)}>Delete</button>
+                                </td>
+                            </tr>
+                        </>
+                    )
+                })}
+            </tbody>
+        </table>
+      )}
+    </div>
+  )
+}
+
+export default AllProducts
